e91d0e928a7f0572cb6b530791380e2444e0cb4d
hope <tasanbannu@gmail.com>
2023-03-23 10:31:43 -0400

    Add movie hover effect

--- movie/src/components/MoviesList.js
+++ movie/src/components/MoviesList.js
@@ -4,7 +4,7 @@ function MoviesList(props) {
   return (
     <div className="grid grid-cols-4 gap-5">
       {props.movies.map((movie, index) => (
-        <div key={index}>
+               <div key={index} class="relative transform hover:scale-110 transition duration-300">
           <img src={movie.Poster} alt="movie" />
         </div>
       ))}
