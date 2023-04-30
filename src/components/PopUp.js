7b10619c7effc77f5ab704e587f5e9da355bdaab
hope <tasanbannu@gmail.com>
2023-04-30 15:10:16 -0400

    Add PopUp component

---
+++ src/components/PopUp.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Pop(props) {
+  const { Title, Year,  Type } = props.movie;
+  const handleClose=()=>{
+    props.onClose();
+  }
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <div className="modal-details">
+          <h2>{Title}</h2>
+          <p>Year of Publication: {Year}</p>
+          <p>Type: {Type}</p>
+        </div>
+        <div className="button">
+          {" "}
+          <button onClick={handleClose}>Close</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Pop;
